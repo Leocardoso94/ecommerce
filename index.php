f92b149cdc4aedde85d4ba22549570ffa8e74fb0
Leonardo Cardoso <leocardosoti@gmail.com>
2017-05-12 14:57:07 -0300

    Atualizacao index.php

--- index.php
+++ index.php
@@ -4,12 +4,11 @@ require_once("vendor/autoload.php");
 
 $app = new \Slim\Slim();
 
-$app->get('/',function(){
-	echo json_encode($arrayName = array('date' => "10/20", ));
-});
+$app->config('debug',true);
 
-$app->get('/hello/:name', function ($name) {
-	echo "Hello, " . $name;
+
+$app->get('/', function () {
+	echo "Hello";
 });
 
 $app->run();
